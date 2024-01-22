b03a67666e67a74ce528efdec7104cea135060ae
Lynniest <swanmhtetlynn@gmail.com>
2024-01-22 13:48:24 +0630

    space_rm

--- app/reset_password/status/[status]/page.jsx
+++ app/reset_password/status/[status]/page.jsx
@@ -7,7 +7,7 @@ const page = (request) => {
     <div className=" text-2xl flex flex-col items-center justify-center min-h-screen bg-custom-bg dark:bg-gray-900 px-4 text-center sm:px-6 lg:px-8">
       <div className='p-5'>
         { status === 'success' ? 
-      <h1 >Password Changed Successful  ly. Close this tab and login again.</h1> : 
+      <h1 >Password Changed Successfully. Close this tab and login again.</h1> : 
       <h1 >Verification Failed.</h1>
         }
       </div>
