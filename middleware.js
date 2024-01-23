1207c0b1e90ee53787d9287e8b6ec5e60d18343d
Lynniest <swanmhtetlynn@gmail.com>
2024-01-23 15:20:30 +0630

    mdw_restored

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export async function middleware(request) {
+    if (request.method === 'OPTIONS') {
+             return new Response(null, {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
+    }
+
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/api/:path*',
+}
